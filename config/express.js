280678aa55f86871d5bce796599f54d794de8b66
Masmoudi Mohamed Ali <mohammedali.masmoudi@gmail.com>
2018-11-07 01:33:23 +0100

    common routes

--- config/express.js
+++ config/express.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import routes from '../server/routes';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, this is API and I\'m ok!'));
+//app.get('/', (req, res) => res.send('Hello, this is API and I\'m ok!'));
+app.use('/api', routes);
 
 export default app;
--- dist/index.js
+++ dist/index.js
@@ -12,8 +12,23 @@ var _env = require("./config/env");
 
 var _env2 = _interopRequireDefault(_env);
 
+var _mongoose = require("mongoose");
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+_mongoose2.default.connect(_env2.default.db);
+_mongoose2.default.connection.on('error', function () {
+  throw new Error("unable to connect to database: " + _env2.default.db);
+});
+_mongoose2.default.connection.on('connected', function () {
+  console.log("Connected to database: " + _env2.default.db);
+});
+
+if (_env2.default.env === 'development') {
+  _mongoose2.default.set('debug', true);
+}
 _express2.default.listen(_env2.default.port, function () {
   console.log("API Server started and listening on port " + _env2.default.port + " (" + _env2.default.env + ")");
 });
