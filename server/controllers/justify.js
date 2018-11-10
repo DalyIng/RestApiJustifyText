24ad05ad47148c0c45eb8fde362a1a72d5d923e5
Masmoudi Mohamed Ali <mohammedali.masmoudi@gmail.com>
2018-11-10 01:51:59 +0100

    CLEAN

--- server/controllers/justify.js
+++ server/controllers/justify.js
@@ -1,6 +1,6 @@
 import Token from "../models/token";
 import justificationCtrl from "./justificationMethod";
-import config from '../../config/env';
+import config from "../../config/env";
 
 function limit(req, res, next) {
   var text = req.body.text;
@@ -25,40 +25,20 @@ function limit(req, res, next) {
 }
 
 function justify(req, res) {
-  //console.log(req.body.text);
   var token = req.headers.authorization.substring(7);
 
-  Token.findOne(
-    // query
-    { token: token },
-
-    // Only return an object with the "name" and "owner" fields. "_id"
-    // is included by default, so you'll need to remove it if you don't want it.
-    { words: true },
-
-    // callback function
-    (err, result) => {
-      if (err) {
-        console.log("WRONG");
-      } else {
-        console.log(result);
-      }
+  Token.findOne({ token: token }, { words: true }, (err, result) => {
+    if (err) {
+      console.log("WRONG");
+    } else {
+      console.log(result);
     }
-  );
+  });
   var text = req.body.text;
   var words = text.split(" ");
-  //console.log(words);
   var numberWords = text.trim().split(/\s+/).length;
-  //console.log(numberWords);
-  var textLength = text.length;
-  //console.log(textLength);
-  //var justifiedTextList = textJustification(words, 80);
   var justifiedTextList = justificationCtrl.textJustification(words, 80);
-  //console.log(justifiedTextList);
   var justifiedText = justifiedTextList.join("\n");
-  //console.log(justifiedText);
-  //console.log(token);
-  ///////////////////////////////////
   var query = { token: token };
   Token.findOneAndUpdate(
     query,
@@ -74,7 +54,6 @@ function justify(req, res) {
       console.log(doc);
     }
   );
-  ///////////////////////////////////
   res.set("Content-Type", "text/plain");
   res.send(justifiedText);
 }
